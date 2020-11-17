c0e93a0217af10c976d903440a9c9e6a89651f9b
kthatoto <kthatoto@gmail.com>
2020-11-17 11:14:43 +0900

    Websocket files

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"./server"
+)
+
+func main() {
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
