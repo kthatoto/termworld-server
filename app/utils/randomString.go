351d499e2cb4ae41c030a4af77b838084506158b
kthatoto <kthatoto@gmail.com>
2020-11-20 03:25:44 +0900

    Add random string util

---
+++ app/utils/randomString.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+)
+
+func RandomString(length int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
