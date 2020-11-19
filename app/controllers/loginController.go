1fe163799fc14aecc4ca525aad664d158f23c7ca
kthatoto <kthatoto@gmail.com>
2020-11-19 14:56:28 +0900

    Fix

--- app/controllers/loginController.go
+++ app/controllers/loginController.go
@@ -1,12 +1,29 @@
 package controllers
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net/http"
 
 	db "github.com/kthatoto/termworld-server/app/database"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func LoginNew(c *gin.Context) {
-	fmt.Println(db.Client)
+	collection := db.Database.Collection("users")
+
+	res, err := collection.InsertOne(context.Background(), bson.M{
+		"Email": "aaaa",
+		"Token": "cccc",
+	})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id": res.InsertedID,
+		"status": "ok",
+	})
 }
--- app/database/init.go
+++ app/database/init.go
@@ -3,22 +3,21 @@ package middlewares
 import (
 	"context"
 	"time"
-	"errors"
 	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var Client *mongo.Client
+var Database *mongo.Database
 
 func init() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
 	defer cancel()
-	err := errors.New("")
-	Client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@mongo:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	Database = client.Database("termworld_development")
 }
