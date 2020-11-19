3c52b984f5ca24c351270a35ec0fb2320cc02ec1
kthatoto <kthatoto@gmail.com>
2020-11-19 15:21:10 +0900

    Change db password

--- app/database/init.go
+++ app/database/init.go
@@ -14,7 +14,7 @@ var Database *mongo.Database
 func init() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:rootroot@localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
 		return
