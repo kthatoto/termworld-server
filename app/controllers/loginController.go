858591a03cc428ebdd5fc91c95a679848b206e72
kthatoto <kthatoto@gmail.com>
2020-11-20 01:32:11 +0900

    login new controller

--- app/controllers/loginController.go
+++ app/controllers/loginController.go
@@ -2,28 +2,43 @@ package controllers
 
 import (
 	"context"
-	"log"
 	"net/http"
 
-	db "github.com/kthatoto/termworld-server/app/database"
 	"github.com/gin-gonic/gin"
+	"github.com/goware/emailx"
+	db "github.com/kthatoto/termworld-server/app/database"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func LoginNew(c *gin.Context) {
-	collection := db.Database.Collection("users")
+type loginNewRequestJson struct {
+	Email string `json:"email"`
+}
 
-	res, err := collection.InsertOne(context.Background(), bson.M{
-		"email": "aaaa",
-		"token": "cccc",
-	})
-	if err != nil {
-		log.Fatal(err)
+func generateTokenString() {
+	return "aaaabbbbcccc"
+}
+
+func LoginNew(c *gin.Context) {
+	var data loginNewRequestJson
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
+		return
+	}
+	if err := emailx.Validate(data.Email); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{
-		"id": res.InsertedID,
-		"status": "ok",
-	})
+	collection := db.Database.Collection("users")
+	res, err := collection.UpdateOne(
+		context.Background(),
+		bson.M{ "email": data.Email },
+		bson.M{ "token": generateTokenString() },
+		mongo.UpdateOptions{ Upsert: true },
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{ "error": err.Error() })
+		return
+	}
 }
