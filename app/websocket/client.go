e23fe0502ebd918fb695c65569e86f883444701b
kthatoto <kthatoto@gmail.com>
2020-11-24 01:08:51 +0900

    Add write pump

--- app/websocket/client.go
+++ app/websocket/client.go
@@ -2,6 +2,8 @@ package websocket
 
 import (
 	"time"
+	"bytes"
+	"log"
 
 	ws "github.com/gorilla/websocket"
 )
@@ -46,4 +48,41 @@ func (client *Client) readPump(hub *Hub) {
 }
 
 func (client *Client) writePump(hub *Hub) {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		client.conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-client.send:
+			client.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				client.conn.WriteMessage(ws.CloseMessage, []byte{})
+				return
+			}
+
+			writer, err := client.conn.NextWriter(ws.TextMessage)
+			if err != nil {
+				return
+			}
+			writer.Write(message)
+
+			n := len(client.send)
+			for i := 0; i < n; i++ {
+				writer.Write(newline)
+				writer.Write(<-client.send)
+			}
+
+			if err := writer.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			client.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
 }
