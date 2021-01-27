43ac69fc0b38bf376547f678f06e6c8444b2b36e
kthatoto <kthatoto@gmail.com>
2021-01-28 08:45:21 +0900

    Error handle for move command

--- game/command/commands/move.go
+++ game/command/commands/move.go
@@ -8,10 +8,16 @@ import (
 )
 
 func Move(player *models.Player, resp *Response, options []string) error {
+	if !player.Live {
+		return errors.New(fmt.Sprintf("プレイヤー：%s は起動していません。まずstartコマンドで起動させてください", player.Name))
+	}
+	if len(options) == 0 {
+		return errors.New("moveする方向が必要です (up, down, left, right)")
+	}
+
 	direction := options[0]
-	fmt.Printf("%s move command recieved!", direction)
 	if !(direction == "up" || direction == "down" || direction == "left" || direction == "right") {
-		return errors.New(fmt.Sprintf("[%s] is invalid direction (The direction should be [up, down, left or right])", direction))
+		return errors.New(fmt.Sprintf("%s は不正な方向です。up, down, left, rightから入力してください", direction))
 	}
 	return nil
 }
